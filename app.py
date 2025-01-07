2bde4da95799c61f4e49f55efe018998a6eb7e44
Mike Corey <michael.corey@us.af.mil>
2025-01-07 08:54:37 -0600

    port is 8080 or set by env var.  (debug off)

--- app.py
+++ app.py
@@ -108,4 +108,6 @@ def delete(id):
         return jsonify({'result': 'not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
